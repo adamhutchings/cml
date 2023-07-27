e8037e4e4e006796e26ba6400bdcead5098e17cb
adamhutchings <adam.abahot@gmail.com>
2023-07-27 13:59:07 -0400

    Remove old testing code

--- src/main.c
+++ src/main.c
@@ -7,42 +7,6 @@
 #include "model.h"
 
 /**
- * Make sure that the intermediate sizes are as expected.
- */
-int nntest() {
-    struct cmlneuralnet net;
-    cmlninit(&net, 100, 1, 3);
-    struct cmlvector in, out;
-    cmlvinit(&in, 100);
-    cmlnrandinit(&net);
-    /* Test 1 million applications. */
-    time_t start, end;
-    time(&start);
-    int trials = 1000000;
-    int pct = trials / 100;
-    for (int i = 0; i < trials; ++i) {
-        if (i % pct == pct - 1) {
-            printf("%d%% of the way done.\n", (i + 1) / pct);
-        }
-        cmlnapp(&net, &in, &out);
-    }
-    time(&end);
-    double avgtime = (end - start) / (double) trials;
-    printf("Average time to run neural net is %f seconds.\n", avgtime);
-    int totalentries = 0;
-    for (int i = 0; i < net.layers; ++i) {
-        totalentries += net.matrices[i].m * net.matrices[i].n;
-        totalentries += net.biases[i].len;
-    }
-    printf("Total parameters in the net: %d\n", totalentries);
-    printf("Time to complete one learning cycle using epsilon method: %f seconds.\n", avgtime * (double) totalentries);
-    cmlvfree(&in);
-    cmlvfree(&out);
-    cmlnfree(&net);
-    return 0;
-}
-
-/**
  * Test the basics of a model.
  */
 int modeltest() {
