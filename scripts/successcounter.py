7798311781069b5303d5524cfae145394d375efb
adamhutchings <adam.abahot@gmail.com>
2023-07-30 23:20:40 -0400

    Modify for variable speed

--- scripts/successcounter.py
+++ scripts/successcounter.py
@@ -2,13 +2,13 @@ import subprocess
 
 s = 0
 
-for i in range(100):
+for i in range(1000):
 
     process = subprocess.Popen(["bash", "scripts/drun.sh"], stdout=subprocess.PIPE)
     result = process.communicate()[0]
     if 'Passed' in str(result):
         s += 1
 
-    print(f'Finished run {i + 1} of 100.')
+    print(f'Finished run {i + 1} of {s}.')
 
-print(f'Success rate: {s}%.')
+print(f'Success rate: {round(s/10, 2)}%.')
