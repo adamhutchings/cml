3512db51428ae0a44f90c93fd3787b8e46ad6eeb
adamhutchings <adam.abahot@gmail.com>
2023-07-30 23:26:10 -0400

    Make stats accurate

--- scripts/successcounter.py
+++ scripts/successcounter.py
@@ -1,14 +1,15 @@
 import subprocess
 
 s = 0
+runs = 100
 
-for i in range(1000):
+for i in range(runs):
 
     process = subprocess.Popen(["bash", "scripts/drun.sh"], stdout=subprocess.PIPE)
     result = process.communicate()[0]
     if 'Passed' in str(result):
         s += 1
 
-    print(f'Finished run {i + 1} of {s}.')
+    print(f'Finished run {i + 1} of {runs}.')
 
-print(f'Success rate: {round(s/10, 2)}%.')
+print(f'Success rate: {round(s/(runs/100), 2)}%.')
