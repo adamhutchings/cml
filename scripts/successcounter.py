697817a79b96b983069a87875687eb9df12803c5
adamhutchings <adam.abahot@gmail.com>
2023-08-01 00:45:57 -0400

    Make the counter UI better

--- scripts/successcounter.py
+++ scripts/successcounter.py
@@ -11,6 +11,16 @@ except ValueError:
     print(f'Argument {sys.argv[1]} was not a valid number of runs.')
     sys.exit()
 
+pbarlen = 50
+efruns = runs + pbarlen - (runs % pbarlen)
+if runs % pbarlen == 0:
+    efruns = runs
+
+print(f'Progress:\n|{"-" * pbarlen}|')
+print(f'|', end='', flush=True)
+
+ps = 0
+
 for i in range(runs):
 
     process = subprocess.Popen(["bash", "scripts/drun.sh"], stdout=subprocess.PIPE)
@@ -18,6 +28,9 @@ for i in range(runs):
     if 'Passed' in str(result):
         s += 1
 
-    print(f'Finished run {i + 1} of {runs}.')
+    if (s - 1) % (efruns // pbarlen) == 0:
+        print(f'-', end='', flush=True)
+        ps += 1
 
+print(f'{"-" * (pbarlen - ps)}|')
 print(f'Pass rate: {s} out of {runs} tests.')
